575fd779f0e31b4b4de2693401019425bcd0c19b
Krisha-2705 <145978458+Krisha-2705@users.noreply.github.com>
2024-10-21 18:42:13 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function pressp () {
+document.getElementById ('ans').innerHTML= Number (document.getElementById ('num1').value )+ Number(document.getElementById ('num2').value) 
+}
+
+function pressm () {
+document.getElementById ('ans').innerHTML=document.getElementById ('num1').value - document.getElementById ('num2').value
+}
+
+function presst () {
+document.getElementById ('ans').innerHTML=document.getElementById ('num1').value * document.getElementById ('num2').value
+}
+
+function pressd () {
+document.getElementById ('ans').innerHTML=document.getElementById ('num1').value / document.getElementById ('num2').value
+}
